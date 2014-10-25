5f50998e0bf49403d0bbe24f8930cbf7c1a72d6d
thejdeep <tejdeepg@gmail.com>
2014-10-25 16:31:14 +0530

    Changes

--- js/screens/play.js
+++ js/screens/play.js
@@ -4,6 +4,8 @@ game.PlayScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {
 		// reset the score
+		 me.levelDirector.loadLevel("area01");
+             
 		game.data.score = 0;
 
 		// add our HUD to the game world
